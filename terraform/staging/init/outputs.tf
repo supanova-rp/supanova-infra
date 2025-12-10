2357a8b44dda16d0c3ad44a51f7b66ed576b35cc
Jamie Garner <jamiegarner123@gmail.com>
2025-12-10 16:36:31 +1100

    remove staging

--- terraform/staging/init/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "supanova_infra_access_key_id" {
-  value = aws_iam_access_key.supanova_infra_staging_user_key.id
-}
-
-output "supanova_infra_secret_access_key" {
-  value     = aws_iam_access_key.supanova_infra_staging_user_key.secret
-  sensitive = true
-}
