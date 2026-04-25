0a6078bba9d8624c0648aace83a6f8db5b845990
Jamie Garner <jamiegarner123@gmail.com>
2026-04-26 09:13:14 +1000

    add s3 backend for all envs

--- terraform/demo/main/provider.tf
+++ terraform/demo/main/provider.tf
@@ -5,6 +5,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+  required_version = ">= 1.10.0"
+
+  backend "s3" {
+    bucket       = "supanova-tfstate"
+    key          = "demo/main/terraform.tfstate"
+    region       = "eu-west-2"
+    profile      = "terraform"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
